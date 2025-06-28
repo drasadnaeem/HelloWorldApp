62ebf1f4ab44431e407fac25ce1d58480a683f2a
Dr. Asad Naeem <drasadnaeem@gmail.com>
2025-06-28 18:38:57 +0800

    Version 1.0.0.2

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Squirrel;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Squirrel;
 
 namespace HelloWorldApp
 {
@@ -20,6 +22,45 @@ namespace HelloWorldApp
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Hello, World! New Version " + Application.ProductVersion);
+        }
+
+        
+
+    private async void CheckForUpdates()
+        {
+            try
+            {
+                using (var mgr = await UpdateManager.GitHubUpdateManager("https://github.com/drasadnaeem/HelloWorldApp"))
+                {
+                    var updateInfo = await mgr.CheckForUpdate();
+
+                    if (updateInfo.ReleasesToApply.Any())
+                    {
+                        DialogResult result = MessageBox.Show(
+                            "A new update is available. Would you like to download and install it now?",
+                            "Update Available",
+                            MessageBoxButtons.YesNo,
+                            MessageBoxIcon.Question
+                        );
+
+                        if (result == DialogResult.Yes)
+                        {
+                            await mgr.UpdateApp();
+                            MessageBox.Show("Update installed. Please restart the application.");
+                            Application.Restart();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Update check failed: " + ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForUpdates();
         }
     }
 }
